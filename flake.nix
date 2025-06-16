19c1b6ae912c6ebca4cad0d02aa32b57e7864799
adjoly <contact@adjoly.fr>
2025-06-16 10:43:48 +0200

    「🏗️」 wip: added a flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell.override { } {
+            buildInputs = with pkgs; [
+
+            ];
+            packages = with pkgs; [
+              nixd
+              nixfmt-rfc-style
+
+              nodejs_22
+              pnpm
+            ];
+          };
+        }
+      );
+    };
+}
