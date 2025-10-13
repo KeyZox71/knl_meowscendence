d15161dba4dc676a449282cfbcb49ca5baf89ab8
adjoly <contact@adjoly.fr>
2025-10-13 15:10:54 +0200

    「✨」 feat: now creating user on the user api when registering

---
+++ src/utils/authUserCreate.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+/**
+ * @param {string} username
+ * @param {import('fastify').FastifyInstance} fastify
+ */
+export async function authUserCreate(username, fastify) {
+    const payload = {
+        displayName: username,
+    };
+    const cookie = fastify.jwt.sign({ user: "admin" });
+
+	const url = process.env.USER_URL || "http://localhost:3002/"
+
+    await axios.post(
+        "http://localhost:3002/users/" + username,
+        payload,
+        {
+            headers: {
+                'Cookie': 'token=' + cookie,
+            },
+        }
+    );
+}
