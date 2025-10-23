9ea154492c7fea56428b67f87fccf4de50ff38d4
adjoly <contact@adjoly.fr>
2025-10-23 17:35:38 +0200

    「🔨」 fix: fixed some things.

--- src/api/user/gUser.js
+++ src/api/user/gUser.js
@@ -1,9 +1,6 @@
 export async function gUser(request, reply, fastify, getUserInfo) {
 	try {
 		const userId = request.params.userId;
-		if (request.user !== userId && request.user !== 'admin') {
-			return reply.code(401).send({ error: 'Unauthorized' });
-		}
 		if (!getUserInfo.get(userId)) {
 			return reply.code(404).send({ error: "User does not exist" });
 		}
